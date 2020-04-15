b476ceccde4b85d1d77b5da7ddd9a2f69e2b43ee
Benjamin Pierce <bcpierce00t@gmail.com>
2020-04-15 12:14:14 -0400

    Little bit of progress on

--- formal.v
+++ formal.v
@@ -1,12 +1,32 @@
 Section foo.
 
+(* TODO: Make all this match the terminology in the .tex -- e.g., a
+   Contour should correspond to a MachineState, not to a Trace,
+   etc. *)
+
 Variable MachineState : Type.
-Variable Trace : Type.
+Variable step : MachineState -> option MachineState.
+
+CoInductive Trace : Type :=
+  last : MachineState -> Trace
+| notlast : MachineState -> Trace -> Trace.
+
+Definition fst (MM : Trace) : MachineState :=
+  match MM with
+  | last M => M
+  | notlast M _ => M
+  end.
+
+CoFixpoint traceOf (M : MachineState) : Trace :=
+  match step M with
+  | None => last M
+  | Some M' => notlast M (traceOf M')
+  end.
+
+
 Variable Contour : Type.
 Variable StackIntegrity : Trace -> Contour -> Prop.
-Variable fst : Trace -> MachineState.
 Variable StackConfidentiality : Trace -> Trace -> Contour -> Prop.
-Variable traceOf : MachineState -> Trace.
 Variable subtrace : Trace -> Trace -> Contour -> Prop.
 Variable variantOf : MachineState -> MachineState -> Contour -> Prop.
 
