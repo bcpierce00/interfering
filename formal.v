dd97b639872fbd69ec71ca452aae78d19ab9918d
Benjamin Pierce <bcpierce00t@gmail.com>
2020-04-15 12:00:11 -0400

    First try at formal.v

--- formal.v
+++ formal.v
@@ -1,2 +1,23 @@
-Coinductive StackSafety :=
-end.
+Section foo.
+
+Variable MachineState : Type.
+Variable Trace : Type.
+Variable Contour : Type.
+Variable StackIntegrity : Trace -> Contour -> Prop.
+Variable fst : Trace -> MachineState.
+Variable StackConfidentiality : Trace -> Trace -> Contour -> Prop.
+Variable traceOf : MachineState -> Trace.
+Variable subtrace : Trace -> Trace -> Contour -> Prop.
+Variable variantOf : MachineState -> MachineState -> Contour -> Prop.
+
+CoInductive StackSafety : Trace -> Contour -> Prop :=
+  ss : forall (MM : Trace) (C : Contour),
+       (StackIntegrity MM C) *
+       (forall N, variantOf N (fst MM) C ->
+                  StackConfidentiality MM (traceOf N) C) *
+       (forall Mcallee C',
+           subtrace MM Mcallee C' -> StackSafety Mcallee C')
+       ->
+       StackSafety MM C.
+
+End foo.
